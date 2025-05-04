5bbeafef707c871e57b68fa7b2fba9535edbcf38
noonisschoolofartssafey <noonisschoolofartssafey@gmail.com>
2025-05-04 05:46:52 +0000

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyDwlDnuT0FS0Y8XIMotGzN_LOE7bsvwoew',
+      authDomain: 'notebook-ai-app-33187.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'notebook-ai-app-33187',
+      storageBucket: 'notebook-ai-app-33187.firebasestorage.app',
+      messagingSenderId: '582872056632',
+      appId: '1:582872056632:web:55c5eb058743cb446c77cd',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyDLGZPm0-mrOKah2wtZ3dxx7lfxuIDL2oM',
+  debug_token: ''
+};
